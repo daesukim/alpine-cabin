88fd02e6e2641faf1b027348bb73431d2695d8bd
Daesu <rlaeotn6@gmail.com>
2026-05-11 09:41:15 -0400

    add a base structure

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+
+const headingStyles = {
+  h1: css`
+    font-size: 3rem;
+    font-weight: 600;
+  `,
+  h2: css`
+    font-size: 2rem;
+    font-weight: 600;
+  `,
+  h3: css`
+    font-size: 2rem;
+    font-weight: 500;
+  `,
+};
+
+const Heading = styled.h1`
+  ${(props) => headingStyles[props.as]}
+  line-height: 1.4;
+`;
+
+export default Heading;
